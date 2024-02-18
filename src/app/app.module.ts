af3ca11fe1d7701a97ec72676b63c837edce4cfd
Bramo033 <ramos.brenna.142@gmail.com>
2024-02-18 01:57:03 -0500

    Add Dashboard and Table to Nav and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,62 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Importing Angular Material Components
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu'; 
+
+// Routing Definitions
+const routes: Routes = [
+  { path: 'dash', component: DashComponent },
+  { path: 'table', component: TableComponent },
+  { path: '', redirectTo: '/dash', pathMatch: 'full' }, // redirect empty path to '/about'
+  { path: '**', redirectTo: '/dash' }, // wildcard route back home
+  // more routes here
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    TableComponent,
+    DashComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    // Material Components
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
