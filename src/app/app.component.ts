72ca474b9fb913f31823518a30b9b622f07cd542
Bramo033 <ramos.brenna.142@gmail.com>
2024-07-16 20:08:14 -0400

    Update import for environment keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { environment } from '../environments/environments';
 
 @Component({
   selector: 'app-root',
