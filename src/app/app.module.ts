5b9cf71e09024e98b76885cc883e911e6f884d9d
Bramo033 <ramos.brenna.142@gmail.com>
2024-07-23 16:12:42 -0400

    Install pwa after installing service worker would help the service register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -67,7 +67,13 @@ const routes: Routes = [
         MatGridListModule,
         MatCardModule,
         MatMenuModule,
-        MatInputModule], 
+        MatInputModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })], 
         providers: [
           provideHttpClient(withInterceptorsFromDi()),
           { provide: LocationStrategy, useClass: HashLocationStrategy }
